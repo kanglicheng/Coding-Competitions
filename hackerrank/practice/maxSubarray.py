89ac52a53fad1cf4e53cb5986025eed5863c895f
Kang-Li (Stephen) Cheng <kangli.cheng@gmail.com>
2017-04-15 19:49:55 -0400

    Update maxSubarray.py

--- hackerrank/practice/maxSubarray.py
+++ hackerrank/practice/maxSubarray.py
@@ -15,3 +15,32 @@ for _ in range(int(input())):
         if a[i] > 0:
             ans2 += a[i]
     print(ans, ans2)
+
+    """
+    def MaxSub(A,N):
+    max_ending_here = max_so_far = 0
+    for x in A:
+        max_ending_here = max(0, max_ending_here + x)
+        max_so_far = max(max_so_far, max_ending_here)
+    if ( max_so_far == 0 ):
+        max_so_far = max(A)
+    return str( max_so_far )
+    
+def MaxSubNonCont(A,N):
+    tot = 0
+    for x in A:
+        if (x > 0):
+            tot += x
+    if (tot == 0):
+        tot = max(A)
+    return str( tot )
+    
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    A = [int(i) for i in input().split()]
+    print( MaxSub(A,N) + " " + MaxSubNonCont(A,N) )
+    
+
+      """
+      
